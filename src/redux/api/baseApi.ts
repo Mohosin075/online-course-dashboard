91ac13d757050d6870b16b1d451e7714c076a4de
Mohosin <mohosinali075@gmail.com>
2025-01-23 00:17:01 +0600

    redux setup

---
+++ src/redux/api/baseApi.ts
@@ -0,0 +1,61 @@
+// Need to use the React-specific entry point to import createApi
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../store";
+
+// Define a service using a base URL and expected endpoints
+export const baseApi = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:5000",
+    // credentials: "include",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+
+      if (token) {
+        headers.set("authorization", `${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["Classes"],
+  endpoints: (builder) => ({
+    registerUser: builder.mutation({
+      query: ({ userData }) => {
+        return {
+          url: `/users/register`,
+          method: "POST",
+          body: userData,
+        };
+      },
+    }),
+    addUser: builder.mutation({
+      query: ({ userData }) => {
+        return {
+          url: `/users`,
+          method: "POST",
+          body: userData,
+        };
+      },
+      invalidatesTags: ["Classes"],
+    }),
+    deleteProduct: builder.mutation({
+      query: ({ productId }) => {
+        return {
+          url: `/products/${productId}`,
+          method: "DELETE",
+        };
+      },
+      invalidatesTags: ["Classes"],
+    }),
+    getClasses: builder.query({
+      query: () => `/allClasses`,
+      providesTags: ["Classes"],
+    }),
+  }),
+});
+
+export const {
+  useDeleteProductMutation,
+  useRegisterUserMutation,
+  useAddUserMutation
+} = baseApi;
